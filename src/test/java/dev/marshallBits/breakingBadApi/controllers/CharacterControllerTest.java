99e36f943d47fc7861ac234f2b0d2d76989bd8dd
Juan Soto Segovia <juanjesussotos@gmail.com>
2025-10-19 01:38:10 -0300

    Solucion retobit-10

--- src/test/java/dev/marshallBits/breakingBadApi/controllers/CharacterControllerTest.java
+++ src/test/java/dev/marshallBits/breakingBadApi/controllers/CharacterControllerTest.java
@@ -11,10 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -53,5 +51,54 @@ public class CharacterControllerTest {
                 .andExpect(jsonPath("$.occupation").value("Lawyer"));
 
 
+    }
+
+
+    //Tarea 1: GET /api/characters/{id}
+    @Test
+    @DisplayName("GET: Recibimos a Walter White por ID")
+    void getCharacterByID() throws Exception {
+        mockMvc.perform(get("/api/characters/{id}", 1))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.name").value("Walter White"))
+                .andExpect(jsonPath("$.occupation").value("Profesor de Química"))
+                .andExpect(jsonPath("$.status").value("ALIVE"));
+    }
+
+    // Tarea 2: PATCH /api/characters/{id}/status
+    @Test
+    @DisplayName("PATCH: Cambia estado de Walter White a DEAD")
+    void patchStatusCharacterById() throws Exception {
+        String jsonUpdate = """
+            { "status": "DEAD" }
+            """;
+        mockMvc.perform(patch("/api/characters/{id}/status", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonUpdate)
+        )
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.name").value("Walter White"))
+                .andExpect(jsonPath("$.status").value("DEAD"));
+    }
+
+    // Tarea 3: PUT /api/characters/{id}
+    @Test
+    @DisplayName("PUT: Modificacion de datos Walter White")
+    void putUpdateCharacterById() throws Exception {
+        String jsonUpdate = """
+                {
+                    "name": "Nelson Mons",
+                    "occupation": "Delivery",
+                    "status": "DEAD"
+                }
+                """;
+        mockMvc.perform(put("/api/characters/{id}", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonUpdate)
+        )
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.name").value("Nelson Mons"))
+                .andExpect(jsonPath("$.occupation").value("Delivery"))
+                .andExpect(jsonPath("$.status").value("DEAD"));
     }
 }
